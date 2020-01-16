b625e007eb6f855207749e493f240881ab7be3f4
Jonathan Spratte <jspratte@yahoo.de>
2020-01-16 03:00:26 +0100

    added benchmark scripts

---
+++ Benchmarks/read_results.py
@@ -0,0 +1,56 @@
+from sys import argv
+import numpy as np
+import re
+
+def read_results(name, block_size=None):
+    num_line = re.compile("^Number of Keys: (\d+)$")
+    pkg_line = re.compile("^=== Benchmarking (.*) ===$")
+    ops_line = re.compile("^\d\S* seconds \((\d\S*) ops\)$")
+    data = {}
+    with open(name, 'r') as fp:
+        while True:
+            line = fp.readline()
+            if line == '': break
+            elif None != (match := num_line.match(line)):
+                try:
+                    num = int(match.group(1))
+                except:
+                    raise ValueError("Could not convert" + match.group(1))
+            elif None != (match := pkg_line.match(line)):
+                pkg = match.group(1)
+            elif None != (match := ops_line.match(line)):
+                if pkg not in data: data[pkg] = [[],[]]
+                data[pkg][0].append(num)
+                try:
+                    data[pkg][1].append(float(match.group(1)))
+                except:
+                    raise ValueError("Could not convert" + match.group(1))
+        data_keys = iter(data)
+        num = data[next(data_keys)][0]
+        for i in data_keys:
+            if num != data[i][0]: return None, data
+        num = np.array(num)
+        ops = {}
+        for i in data:
+            ops[i] = np.array(data[i][1])
+        return num, ops
+    return None, None
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage:", argv[0], "<filename>")
+        quit(1)
+    num, ops = read_results(argv[1])
+    if ops == None:
+        print("Some bad error happened")
+        quit(2)
+    if num == None:
+        for i in ops:
+            print(i)
+            print("  grad=1", np.polyfit(ops[i][0], ops[i][1], 1))
+            print("  grad=2", np.polyfit(ops[i][0], ops[i][1], 2))
+    else:
+        for i in ops:
+            print(i)
+            print("  grad=1", np.polyfit(num, ops[i], 1))
+            print("  grad=2", np.polyfit(num, ops[i], 2))
---
+++ Benchmarks/run_benchmark.sh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+
+## settings for this script
+local results="benchmark_results.txt"
+local tmp="benchmark_tmp"
+
+## settings for benchmarks.tex
+# target time of a single \benchmark:n
+local target=1
+# number of \benchmark:n calls per package
+local repetitions=30
+
+local predefs="\\def\\keytgt{$target}\\def\\keyrep{$repetitions}"
+
+function backup () {
+}
+
+function run_single () {
+    local keyarg=" height = 6 "
+    for (( j=2; j<=$1; j++ )); do
+        keyarg+=", height = 6 "
+    done
+    pdflatex -jobname=$tmp \
+        "$predefs\\def\\keyarg{$keyarg}\\input{benchmarks.tex}"
+    echo "Number of Keys: $1" >> $results
+    grep -A$repetitions '^=== Benchmarking' $tmp.log >> $results
+}
+
+## back up the old results
+mv $results $results.$(date +%y-%m-%d_%H:%M:%S).bak
+
+cp ../expkv.dtx .
+pdftex expkv.dtx
+rm expkv.dtx
+
+for (( i=1; i<=20; i++ )); do
+    run_single $i
+done
+
+echo "\n\n\n### Results ###"
+python3 read_results.py $results
+
+rm expkv.sty expkv.tex expkv.log
+rm $tmp.*
