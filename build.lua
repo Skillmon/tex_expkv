83acf7d073d3255b5b04403007731305cb1e6e41
Jonathan Spratte <jspratte@yahoo.de>
2020-02-27 00:26:40 +0100

    updated tags

--- build.lua
+++ build.lua
@@ -1,7 +1,7 @@
 -- Build script for expkv
 module     = "expkv"
 pkgversion = "0.5"
-pkgdate    = "2020-02-26"
+pkgdate    = "2020-02-27"
 
 -- update package date and version
 tagfiles = {"expkv.dtx", "README.md", "CTAN.md"}
