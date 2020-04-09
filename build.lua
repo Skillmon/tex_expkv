813e1d0fb7c235e38ff56cc53c223ba2a0165468
Jonathan Spratte <jspratte@yahoo.de>
2020-04-09 14:46:38 +0200

    fixed splitting at active equal bug and added tests for it

--- build.lua
+++ build.lua
@@ -1,7 +1,7 @@
 -- Build script for expkv
 module     = "expkv"
-pkgversion = "1.0"
-pkgdate    = "2020-04-07"
+pkgversion = "1.1"
+pkgdate    = "2020-04-09"
 
 -- update package date and version
 tagfiles = {"expkv.dtx", "README.md", "CTAN.md"}
